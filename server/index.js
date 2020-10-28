1fcba7b69525ec16cd44b6d1952d9ee464cd3b8e
Mario90 <mgpinan1990@gmail.com>
2020-10-28 18:02:28 +0000

    installed express session

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const session = require("express-session")
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcrypt");
 
 const app = express();
 const port = 3001;
@@ -25,23 +26,18 @@ const corsOptions = {
   },
 };
 
-app.use(helmet());
 app.use(cors(corsOptions));
+app.use(session({
+  cookie: {
+    maxAge: 2
+  }
+}))
 
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
 });
 
 //UserRoutes
-
-app.get("/users", userQueries.getUsers);
-
-app.get("/user/:id", (req, res, next) => {
-  console.log(req);
-  userQueries.findUserByEmail(req.body.email).then((user) => {
-    console.log(user);
-  });
-});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
